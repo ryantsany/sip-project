fa299626e6e7d2c5f52290d7f740d09fe9ec0623
ryantsany <ryantsany2003@gmail.com>
2025-12-18 14:34:06 +0700

    feat: update docker-compose for production environment and add cloudflared service; configure CORS settings and adjust .gitignore

--- sip-backend/bootstrap/app.php
+++ sip-backend/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
